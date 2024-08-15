ca9608d9b54aae19da318df63956ae4d325be3f8
Lucas Machado <lukaasmachado143@gmail.com>
2024-08-14 23:01:15 -0300

    feat: creting login layout

--- app/lib/pages/login_page.dart
+++ app/lib/pages/login_page.dart
@@ -5,8 +5,42 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
- 
+    return const Scaffold(
+      backgroundColor: Color.fromRGBO(224, 224, 224, 1),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 50,
+              ),
+              Icon(
+                Icons.lock,
+                size: 100,
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Text(
+                'Welcome back, you\'ve been missed !',
+                style: TextStyle(
+                    color: Color.fromRGBO(97, 97, 97, 1), fontSize: 16),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white)),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Color.fromARGB(255, 175, 175, 175)))),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
